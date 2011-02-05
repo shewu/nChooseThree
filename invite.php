7a390fd3242be7d02091c470fd67f099d6a48040
Brian Hamrick <brian.c.hamrick@gmail.com>
2011-02-05 04:22:52 -0500

    Fixed invite email issues

--- invite.php
+++ invite.php
@@ -1,4 +1,5 @@
 <?php
+require("web_vars.php");
 $invite_text_format = "Hey %s,
 
 Thanks for signing up for nChooseThree, an MIT-exclusive service that lets you match up your friends, and check out matches your friends have suggested for you! To activate your account, please visit
@@ -18,9 +19,13 @@ Cheers,
 nChooseThree";
 
 function invite($mail) {
-	global $from_addr;
+	global $from_addr, $invite_text_format;
 	$mail_title = "Activate your nChooseThree account";
 	$mail_text = sprintf($invite_text_format, $mail, "foo", "bar");
 	mail($mail,$mail_title,$mail_text,"From: ".$from_addr);
+}
+
+if(isset($_POST["email"])) {
+	invite($_POST["email"]);
 }
 ?>
