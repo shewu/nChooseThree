ad8f00cf35adf5c84713eef2ab8ace02a065b8c8
shewu <shewu19@gmail.com>
2011-02-04 23:33:26 -0500

    added footer with html5 logo

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
 <title>nChooseThree</title>
 <style type="text/css">
 /* make all the html5 elements display block */
-header,nav {
+body {
+	color: #222;
+	font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
+}
+
+header,nav,footer {
 	display: block;
 }
 
@@ -17,15 +22,29 @@ header {
 	margin-bottom: 24px;
 	background: url('/static/img/logo.png') no-repeat;
 	padding-left: 36px;
-	text-shadow: #999 0px 2px 1px;
+	text-shadow: #999 0px 1px 1px;
 	font-family: Georgia, 'Times New Roman', Times, serif;
+	font-size: 2em;
+}
+
+footer {
+	margin-top: 24px;
+	border-top: 2px dotted #CCC;
+	background: url('/static/img/5.png') no-repeat;
+	padding-right: 135px;
+	height: 64px;
 }
 </style>
 </head>
 <body>
-<nav><a href="/about">About</a> | <a href="/about#feedback">Feedback</a> | <a
-href="/">Home</a></nav>
-<header><a href="/">nChooseThree</a></header>
+<nav>
+	<a href="/about">About</a> | <a href="/about#feedback">Feedback</a> | <a
+href="/">Home</a>
+</nav>
+<header>
+	<a href="/">nChooseThree</a>
+</header>
+<footer>&copy; bhamrick, shewu, tjhance</footer>
 </body>
 </html>
 
