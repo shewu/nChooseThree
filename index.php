aadd4639541407ff6ade79e1ffafea07292ec06d
shewu <shewu19@gmail.com>
2011-02-04 23:10:43 -0500

    nav and header plus logo

--- index.php
+++ index.php
@@ -2,9 +2,30 @@
 <html lang="en">
 <head>
 <title>nChooseThree</title>
+<style type="text/css">
+/* make all the html5 elements display block */
+header,nav {
+	display: block;
+}
+
+nav {
+	text-align: right;
+}
+
+header {
+	border-bottom: 2px dotted #CCC;
+	margin-bottom: 24px;
+	background: url('/static/img/logo.png') no-repeat;
+	padding-left: 36px;
+	text-shadow: #999 0px 2px 1px;
+	font-family: Georgia, 'Times New Roman', Times, serif;
+}
+</style>
 </head>
 <body>
-<h1>nChooseThree</h1>
+<nav><a href="/about">About</a> | <a href="/about#feedback">Feedback</a> | <a
+href="/">Home</a></nav>
+<header><a href="/">nChooseThree</a></header>
 </body>
 </html>
 
