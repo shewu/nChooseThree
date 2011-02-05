913e516f87486a6d9cf53882c5a6412248d246bc
shewu <shewu19@gmail.com>
2011-02-05 01:21:31 -0500

    argh title is a bit screwed up

--- index.php
+++ index.php
@@ -4,9 +4,40 @@
 <title>nChooseThree</title>
 <style type="text/css">
 /* make all the html5 elements display block */
+html {
+	color: #343434;
+}
+
 body {
+	font-size: 75%;
 	color: #222;
+	background: #fff;
 	font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
+}
+
+a:focus, a:hover {
+	color: #000;
+}
+
+a {
+	color: #254a86;
+	text-decoration: none;
+}
+
+h1,h2,h3,h4,h5,h6 {
+	font-weight: normal;
+	color: #333;
+	font-family: "Helvetical Neue", Arial, Helvetica, sans-serif;
+}
+
+h3 {
+	font-size: 1.5em;
+	line-height: 1;
+	margin-bottom: 1em;
+}
+
+p {
+	margin: 0 0 1.5em;
 }
 
 header,nav,footer {
@@ -15,24 +46,44 @@ header,nav,footer {
 
 nav {
 	text-align: right;
+	font-size: smaller;
+	font-weight: lighter;
+}
+
+nav a {
+	color: #254A86;
+	text-decoration: none;
+}
+
+nav a:hover {
+	color: #222;
 }
 
 header {
 	border-bottom: 2px dotted #CCC;
 	margin-bottom: 24px;
-	background: url('/static/img/logo.png') no-repeat;
-	padding-left: 36px;
+	background: url('static/img/logo.png') no-repeat;
+	padding: 0 0 -50px 32px;
 	text-shadow: #999 0px 1px 1px;
 	font-family: Georgia, 'Times New Roman', Times, serif;
 	font-size: 2em;
 }
 
+header a, header a:visited, header a:active, a:hover {
+	color: #222;
+}
+
 footer {
-	margin-top: 24px;
 	border-top: 2px dotted #CCC;
-	background: url('/static/img/5.png') no-repeat;
-	padding-right: 135px;
+	text-align: center;
+	font-weight: lighter;
+}
+
+.html5logo {
+	margin: 24px auto;
+	background: url('static/img/5.png') no-repeat;
 	height: 64px;
+	width: 165px;
 }
 </style>
 </head>
@@ -44,7 +95,28 @@ href="/">Home</a>
 <header>
 	<a href="/">nChooseThree</a>
 </header>
-<footer>&copy; bhamrick, shewu, tjhance</footer>
+<article>
+<section class="column">
+<div class="blurb">
+<h3>Know <strike>two</strike> three friends who should go on a date?</h3>
+<h3>Which of your friends would make a cute <strike>couple</strike> triple?</h3>
+<p>Use nChooseThree to anonymously make match suggestions, and to get matched up
+by your friends!</p>
+<p><strong>If not all three matchees accept, none of the matchees or the
+matchmaker learn of any other response.</strong> The matchmaker's identity is
+revealed when all three A, B, and C accept the match.</p>
+<p><a href="#">Read more about nChooseThree</a></p>
+</div>
+</section>
+<section class="column">
+</section>
+</article>
+<footer>
+	<div class="html5logo"></div>
+	Logo created with <a href="http://www.latex-project.org/"><img alt="\Latex"
+	src="http://upload.wikimedia.org/wikipedia/commons/9/92/LaTeX_logo.svg"
+	width="38" height="16" style="vertical-align: middle" /></a>
+</footer>
 </body>
 </html>
 
