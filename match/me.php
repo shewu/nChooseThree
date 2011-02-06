08cf8e42a9585b4790cb5a55700442f7c53db88e
shewu <shewu19@gmail.com>
2011-02-06 11:48:44 -0500

    added more stuff to me.php

--- match/me.php
+++ match/me.php
@@ -2,6 +2,27 @@
 include("../header.php");
 ?>
 <article>
+<section id="matchmake-nav" class="column grid_3">
+<ul>
+<li class="nav-highlight"><a href="/nChooseThree/match/me.php">Match Me</a></li>
+<li><a href="/nChooseThree/match/make.php">Matchmaker</a></li>
+</ul>
+</section>
+<section class="column grid_9">
+<div class="notice">
+<strong>Suggested matches</strong> for you are shown below.
+</div>
+<!-- matches -->
+<div class="notice">
+<strong>You can have up to 10 crushes at a time.</strong> A crush can be removed
+after a week has passed.
+</div>
+<!-- crushes -->
+<p>The matchmaker <strong>does not receive any information</strong> unless all
+three matchees accept the match.</p>
+<p>The matchmaker's identity is revealed when all three A, B, and C accept the
+match.</p>
+</section>
 </article>
 <?php
 include("../footer.php");
