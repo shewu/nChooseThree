fd75506544b94a25d81bcfd9159d3fa74448603a
shewu <shewu19@gmail.com>
2011-02-06 22:08:29 -0500

    added google analytics

--- header.php
+++ header.php
@@ -11,6 +11,22 @@
 @import '/nChooseThree/static/css/forms.css';
 @import '/nChooseThree/static/css/typography.css';
 </style>
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-21230749-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+ var ga = document.createElement('script'); ga.type =
+ 'text/javascript'; ga.async = true;
+ ga.src = ('https:' == document.location.protocol ?
+	 'https://ssl' : 'http://www') +
+ '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0];
+ s.parentNode.insertBefore(ga, s);
+ })();
+</script>
 </head>
 <body>
 <div class="full-wrapper">
