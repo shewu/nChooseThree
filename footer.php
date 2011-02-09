743b9c2707fc82fc60c4f11d1d9026e527ab839f
shewu <shewu19@gmail.com>
2011-02-09 12:36:26 -0500

    added link to html5 logo

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<div class="html5logo"></div>
+	<a href="http://www.w3.org/html/logo/"><div class="html5logo"></div></a>
 	Logo created with <a href="http://www.latex-project.org/"><img alt="\Latex"
 	src="/nChooseThree/static/img/latex.png" width="38" height="16" style="vertical-align: middle" /></a>
 </footer>
